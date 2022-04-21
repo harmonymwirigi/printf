cc0b8317675a75ee62800ab175490964406466c9
harmonymwirigi <harmonymwirigi99.com>
2022-04-21 13:56:09 -0700

    message

--- main.h
+++ main.h
@@ -1,6 +1,5 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-#include <stdarg.h>
 #include <stdio.h>
 
 /**
@@ -13,79 +12,6 @@ typedef struct print
 	char *t;
 	int (*f)(va_list);
 } print_t;
-int (*check_format(const char *format))(va_list)
-{
-	int i = 0;
-	print_t p[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"i", print_i},
-		{"d", print_d},
-		{"b", print_b},
-		{"u", print_u},
-		{"o", print_o},
-		{"x", print_x},
-		{"X", print_X},
-		{"p", print_p},
-		{"S", print_S},
-		{"r", print_r},
-		{"R", print_R},
-		{NULL, NULL}
-	};
-	for (; p[i].t != NULL; i++)
-	{
-		if (*(p[i].t) == *format)
-		break;
-	}
-	return (p[i].f);
-}
-
-/**
- *  * _printf - function for format printing
- *   * @format: list of arguments to printing
- *    * Return: Number of characters to printing
- *     */
-int _printf(const char *format, ...)
-{
-	va_list ap;
-	int (*f)(va_list);
-	unsigned int i = 0, counter = 0;
-	if (format == NULL)
-		return (-1);
-	va_start(ap, format);
-	while (format && format[i])
-	{
-		if (format[i] != '%')
-		{
-			_putchar(format[i]);
-			counter++;
-			i++;
-			continue;
-		}
-		else
-		{
-			if (format[i + 1] == '%')
-			{
-				_putchar('%');
-				counter++;
-				i += 2;
-				continue;
-			}
-			else
-			{
-				f = check_format(&format[i + 1]);
-				if (f == NULL)
-					return (-1);
-				1 += 2;
-				counter += f(ap);
-				continue;
-			}
-		}
-		i++;
-	}
-	va_en(ap);
-	return (counter);
-}
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_c(va_list c);
